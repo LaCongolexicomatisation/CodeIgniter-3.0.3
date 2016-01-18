46d0c133b0bc4753276fa81012d4dca99e0a3956
LaurineB <la.baillet@hotmail.fr>
2016-01-18 11:50:50 +0100

    design : ajoutActiviteAgenda

--- application/views/ajoutActiviteAgenda.php
+++ application/views/ajoutActiviteAgenda.php
@@ -1,23 +1,30 @@
 <?php include 'head.php'; ?>
 
 <main>
-    <h2>Ajout d'une activité - <?= $titre ?></h2>
+    <h2>Ajout d'une activitÃ© - <?= $titre ?></h2>
 
     <form method="post" id="ajActiviteAgenda">
         <p><i>Les champs marqu&eacutes par </i><em>*</em> sont <em>obligatoires</em> !</p>
-        <select name="activite">
-            <?php foreach($activites as $activite){ ?>
-            <option value="<?= $activite->idActivite() ?>"><?= $activite->nom() ?></option>
-            <?php } ?>
-        </select>
-        <div>
-            <label for='redondance'>Redondance : </label>
-            <input type="number" value="0" name="redondance" id="redondance" /><label> semaines </label>
-            <input type="hidden" value="<?= $hidden ?>"name="dateDebut" />
-        </div>
-        <div>
-            <input type="submit" value="Ajout de l'activité"/>
-        </div>
+
+        <fieldset class="fieldset-grand">
+            <div class="main">
+                    <select name="activite">
+                        <?php foreach($activites as $activite){ ?>
+                            <option value="<?= $activite->idActivite() ?>"><?= $activite->nom() ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <br />
+            <div>
+                <label for='redondance'>Redondance (semaines) : </label>
+                <input type="number" value="0" name="redondance" id="redondance" />
+                <input type="hidden" value="<?= $hidden ?>"name="dateDebut" />
+            </div>
+                <br />
+            <div>
+                <input type="submit" value="Ajout de l'activitÃ©"/>
+            </div>
+        </fieldset>
     </form>
 </main>
 </body>
