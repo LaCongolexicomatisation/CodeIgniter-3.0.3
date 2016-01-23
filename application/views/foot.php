f31bded2f58481104771b2336f8ef10e3843cb02
LaurineB <la.baillet@hotmail.fr>
2016-01-23 09:03:24 +0100

    design : foot

--- application/views/foot.php
+++ application/views/foot.php
@@ -1,5 +1,11 @@
 <footer>
-<br>
+    <div class="foot">
+        Centre périscolaire de Dalse -
+        18 rue de Beaucourt
+        25230 DASLE -
+        Tél : 03.81.37.43.02 -
+        Mail : perisco.dasle@wanadoo.fr
+    </div>
 </footer>
 
 </body>
