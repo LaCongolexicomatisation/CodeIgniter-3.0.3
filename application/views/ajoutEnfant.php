cbc10c60cd880e9b1cee303d1de50646659fcb85
LaurineB <la.baillet@hotmail.fr>
2016-02-15 11:12:47 +0100

    design : ajout parent et enfant

--- application/views/ajoutEnfant.php
+++ application/views/ajoutEnfant.php
@@ -1,43 +1,45 @@
 <?php include 'head.php'; ?>
-<body>
-<main>
-    <h2>Ajout d'un enfant</h2>
+    <body>
+    <main>
+        <h2>Ajout d'un enfant</h2>
 
-    <form method="post" id="ajEnfant">
-        <p><i>Les champs marqu&eacutes par </i><em>*</em> sont <em>obligatoires</em> !</p>
-        <fieldset>
-            <legend>Information de l'enfant</legend>
-            <div class="main">
-                <div id="groupeNomEnfant">
-                    <label for="nom">Nom <em>*</em>:</label>
-                    <input type="text" placeholder="Nom" id="nom" name="nomEnfant" required autofocus/>
+        <form method="post" id="ajEnfant">
+            <p><i>Les champs marqu&eacutes par </i><em>*</em> sont <em>obligatoires</em> !</p>
+            <fieldset class="fieldset-grand">
+                <legend>Information de l'enfant</legend>
+                <div class="main">
+                    <div>
+                        <label for="nom">Nom <em>*</em>:</label>
+                        <input type="text" placeholder="Nom" id="nom" name="nomEnfant" required autofocus/>
+                    </div>
+                    <br/>
+
+                    <div>
+                        <label for="prenom">Prénom <em>*</em>:</label>
+                        <input type="text" placeholder="Prénom" id="prenom" name="prenomEnfant" required/>
+                    </div>
+                    <br/>
+
+                    <div>
+                        <label for="ddn">Date de naissance <em>*</em>:</label>
+                        <input type="date" placeholder="date" id="ddn" name="ddnEnfant" required/>
+                    </div>
                 </div>
-                <br />
-                <div id="groupePrenomEnfant">
-                    <label for="prenom">Prénom <em>*</em>:</label>
-                    <input type="text" placeholder="Prénom" id="prenom" name="prenomEnfant" required/>
+            </fieldset>
+            <br/>
+            <fieldset class="fieldset-grand">
+                <legend>Parents de l'enfant</legend>
+                <div id="parents">
                 </div>
-                <br />
-                <div id="groupeDdnEnfant">
-                    <label for="ddn">Date de naissance <em>*</em>:</label>
-                    <input type="date" placeholder="date" id="ddn" name="ddnEnfant" required/>
-                </div>
+                Si le parent existe déjà dans la base de données :<br>
+                <a href="javascript:parentExistant()">Ajouter un parent existant</a><br><br>
+                Sinon, veuillez le créer d'abord :<br>
+                <a href="javascript:nouveauParent()">Ajouter un nouveau parent</a>
+            </fieldset>
+            <div id="dSubmitAj">
+                <input type="submit" value="Ajout d'un enfant"/>
             </div>
-        </fieldset>
-        <br/>
-        <fieldset>
-            <legend>Parents de l'enfant</legend>
-            <div id="parents">
-            </div>
-            Si le parent existe déjà dans la base de données :<br>
-            <a href="javascript:parentExistant()">Ajouter un parent existant</a><br><br>
-            Sinon, veuillez le créer d'abord :<br>
-            <a href="javascript:nouveauParent()">Ajouter un nouveau parent</a>
-        </fieldset>
-        <div id="dSubmitAj">
-            <input type="submit" value="Ajout d'un enfant"/>
-        </div>
-    </form>
-</main>
+        </form>
+    </main>
 
 <?php include 'foot.php'; ?>
--- application/views/ajoutParent.php
+++ application/views/ajoutParent.php
@@ -1,53 +1,53 @@
 <?php include 'head.php'; ?>
     <body>
-    <main>
-        <h2>Ajout d'un parent</h2>
+<main>
+    <h2>Ajout d'un parent</h2>
 
-        <form method="post" id="ajParent">
-            <p><i>Les champs marqu&eacutes par </i><em>*</em> sont <em>obligatoires</em> !</p>
-            <fieldset>
-                <legend>Information du parent</legend>
-                <div class="main">
-                    <div >
-                        <label for="nom">Nom <em>*</em>:</label>
-                        <input type="text" placeholder="Nom" id="nom" name="nom" required autofocus/>
-                    </div>
-                    <div>
-                        <label for="prenom">Prénom <em>*</em>:</label>
-                        <input type="text" placeholder="Prénom" id="prenom" name="prenom" required/>
-                    </div>
-                    <div>
-                        <label for="ville">Ville <em>*</em>: </label>
-                        <select id="ville" name="idVille" required>
-                            <?php foreach($villes as $ville){?>
-                                <option value="<?=$ville->id(); ?>"><?=$ville->nom(); ?></option>
-                            <?php }?>
-                        </select>
-                    </div>
-                    <div>
-                        <label for="login">Login <em>*</em>:</label>
-                        <input type="text" placeholder="Login" id="login" name="login">
-                    </div>
-                    <div>
-                        <label for="mdp">Mot de passe <em>*</em>:</label>
-                        <input type="text" placeholder="Mot de passe" id="mdp" name="mdp">
-                    </div>
-                    <div>
-                        <label for="mail">Adresse mail <em>*</em>:</label>
-                        <input type="text" placeholder="Mail" id="mail" name="mail">
-                    </div>
-                    <div>
-                        <label for="tel">Téléphone :</label>
-                        <input type="text" placeholder="Téléphone" id="tel" name="tel">
-                    </div>
+    <form method="post" id="ajParent">
+        <p><i>Les champs marqu&eacutes par </i><em>*</em> sont <em>obligatoires</em> !</p>
+        <fieldset class="fieldset-grand">
+            <legend>Information du parent</legend>
+
+                <div>
+                    <label for="nom">Nom <em>*</em>:</label>
+                    <input type="text" placeholder="Nom" id="nom" name="nom" required autofocus/>
                 </div>
-            </fieldset>
-            <br/>
+                <div>
+                    <label for="prenom">Prénom <em>*</em>:</label>
+                    <input type="text" placeholder="Prénom" id="prenom" name="prenom" required/>
+                </div>
+                <div>
+                    <label for="ville">Ville <em>*</em>: </label>
+                    <select id="ville" name="idVille" required>
+                        <?php foreach ($villes as $ville) { ?>
+                            <option value="<?= $ville->id(); ?>"><?= $ville->nom(); ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div>
+                    <label for="login">Login <em>*</em>:</label>
+                    <input type="text" placeholder="Login" id="login" name="login">
+                </div>
+                <div>
+                    <label for="mdp">Mot de passe <em>*</em>:</label>
+                    <input type="text" placeholder="Mot de passe" id="mdp" name="mdp">
+                </div>
+                <div>
+                    <label for="mail">Adresse mail <em>*</em>:</label>
+                    <input type="text" placeholder="Mail" id="mail" name="mail">
+                </div>
+                <div>
+                    <label for="tel">Téléphone :</label>
+                    <input type="text" placeholder="Téléphone" id="tel" name="tel">
+                </div>
 
-            <div id="dSubmitAj">
-                <input type="submit" value="Ajout du parent"/>
-            </div>
-        </form>
-    </main>
+        </fieldset>
+        <br/>
+
+        <div id="dSubmitAj">
+            <input type="submit" value="Ajout du parent"/>
+        </div>
+    </form>
+</main>
 
 <?php include 'foot.php'; ?>
